cb5fa5c8985f3d979ec0314f928da03a8c294ada
AR Wiley <98679479+AR-Wiley@users.noreply.github.com>
2026-03-14 00:34:27 -0500

    Refactor delete_files.sh for improved error handling
    Added set -euo pipefail for better error handling.

--- delete_files.sh
+++ delete_files.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -euo pipefail
+
 if [[ $EUID -ne 0 ]]; then
         echo "You must be root to run this script..."
         exit 1
@@ -14,9 +16,9 @@ function Delete_Files {
 
         declare -a files
 
-        Files+=("$target"/*)
+        files+=("$target"/*)
 
-        for i in "${Files[@]}"; do
+        for i in "${files[@]}"; do
                 rm  -r -f -v "$i"
         done
 
